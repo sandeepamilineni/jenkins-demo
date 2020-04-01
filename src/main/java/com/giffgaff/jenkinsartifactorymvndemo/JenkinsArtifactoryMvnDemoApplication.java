5d82ad9792e9a6e2c4f243fe284b22e6ea6c5335
sandeep <sandeepamilineni@gmail.com>
2020-04-01 10:29:47 +0530

    jenkins demo

---
+++ src/main/java/com/giffgaff/jenkinsartifactorymvndemo/JenkinsArtifactoryMvnDemoApplication.java
@@ -0,0 +1,13 @@
+package com.giffgaff.jenkinsartifactorymvndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsArtifactoryMvnDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsArtifactoryMvnDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giffgaff/jenkinsartifactorymvndemo/JenkinsArtifactoryMvnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giffgaff.jenkinsartifactorymvndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsArtifactoryMvnDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
